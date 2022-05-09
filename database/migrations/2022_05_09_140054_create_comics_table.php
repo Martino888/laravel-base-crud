c743d0784f8fe28dcc79822d4cc2385e7828decd
Martino888 <maxmartino33@gmail.com>
2022-05-09 17:30:05 +0200

    migration -up

--- database/migrations/2022_05_09_140054_create_comics_table.php
+++ database/migrations/2022_05_09_140054_create_comics_table.php
@@ -15,6 +15,22 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            // 'name'
+            $table->string('name');
+            // 'poster'
+            $table->string('poster');
+            // 'series'
+            $table->string('series');
+            // 'price'
+            $table->float('price');
+            // 'description'
+            $table->text('description');
+            // 'sale_date'
+            $table->date('sale_date');
+            // 'page_count'
+            $table->integer('page_count');
+            // 'rated'
+            $table->smallInteger('rated');
             $table->timestamps();
         });
     }
