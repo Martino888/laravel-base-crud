278341b8bbbf7e30adeefa00ad4a38761321730a
Martino888 <maxmartino33@gmail.com>
2022-05-11 17:28:34 +0200

    inserimento estenzioni in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comics Home</title>
-</head>
-<body>
-    <div>
+
         <a href="{{ route('comics.index') }}"><h1>HOME</h1></a>
-    </div>
 
-</body>
-</html>
 
+        @extends('template.base')
+
+        @section('documentTitle')
+            Home
+        @endsection
+
+        @section('header')
+            @include('partials.header')
+        @endsection
